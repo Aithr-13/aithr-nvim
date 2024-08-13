05ca6f0543db0e954815baa31c414436a20796b1
abellmio <abellmio@gmail.com>
2024-08-13 09:33:59 -0400

    Fixed cmp

--- lua/plugins/lsp-cmp.lua
+++ lua/plugins/lsp-cmp.lua
@@ -26,36 +26,59 @@ return {
 			})
 		end,
 	},
-	{
-		"ErichDonGubler/lsp_lines.nvim",
-		config = function()
-			local lsp_lines = require("lsp_lines")
-			lsp_lines.setup()
-		end,
-	},
 	-- Autocompletion
 	{
 		"hrsh7th/nvim-cmp",
 		event = "InsertEnter",
 		dependencies = {
-			{ "L3MON4D3/LuaSnip" },
+			"hrsh7th/cmp-buffer", -- source for text in buffer
+			"hrsh7th/cmp-path", -- source for file system paths
+			{
+				"L3MON4D3/LuaSnip",
+				-- follow latest release.
+				version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+				-- install jsregexp (optional!).
+				build = "make install_jsregexp",
+			},
+			"saadparwaiz1/cmp_luasnip", -- for autocompletion
+			"rafamadriz/friendly-snippets", -- useful snippets
+			"onsails/lspkind.nvim", -- vs-code like pictograms
 		},
 		config = function()
 			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			local lspkind = require("lspkind")
+			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
-				sources = {
-					{ name = "nvim_lsp" },
+				completion = {
+					completeopt = "menu,menuone,preview,noselect",
 				},
+				sources = cmp.config.sources({
+					{ name = "luasnip" }, -- snippets
+					{ name = "buffer" }, -- text within current buffer
+					{ name = "path" }, -- file system paths
+				}),
+
 				mapping = cmp.mapping.preset.insert({
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-u>"] = cmp.mapping.scroll_docs(-4),
-					["<C-d>"] = cmp.mapping.scroll_docs(4),
+					["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+					["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+					["<C-e>"] = cmp.mapping.abort(), -- close completion window
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
 				}),
 				snippet = {
 					expand = function(args)
-						vim.snippet.expand(args.body)
+						luasnip.lsp_expand(args.body)
 					end,
+				},
+				formatting = {
+					format = lspkind.cmp_format({
+						maxwidth = 50,
+						ellipsis_char = "...",
+					}),
 				},
 			})
 		end,
@@ -107,6 +130,13 @@ return {
 					end,
 				},
 			})
+		end,
+	},
+	{
+		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+		config = function()
+			local lsp_lines = require("lsp_lines")
+			lsp_lines.setup()
 		end,
 	},
 
